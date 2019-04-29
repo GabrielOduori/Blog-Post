bbeeb9fe62f656673b429687a305f20d59351a9c
Gabriel Oduori <gabriel.oduori@gmail.com>
2019-04-29 23:23:47 +0300

    Added CRUD functions to the blogs view file

---
+++ app/blogs/views.py
@@ -0,0 +1,84 @@
+from flask import render_template, url_for, request, redirect
+from flask_login import current_user, login_required
+from app import db
+from app.models import Blog, Post
+from app.blogs.forms import PostForm
+
+
+# CREATE A BLOG
+
+@blog.route('/create', methods = ['GET', 'POST'])
+@login_required
+def create_post():
+    post_form = PostForm()
+    if post_form.validate_on_submit():
+        post = Blog(title = form.title.data,
+                    body = form.body.data,
+                    user_id = current_user.id)
+        db.session.add(post)
+        db.session.commit()
+        flash('Post saved')
+        return redirect(url_for('main.html'))
+    return render_template('blog/create_post.html', post_form=post_form)
+
+
+# VIEW A POST
+@blog.route('blog/<int:post_id>')
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title =post.title, date = post.timestamp, post = post)
+
+# UPDATE
+
+
+@blog.route('blog/<int:post_id>/update', methods = ['GET', 'POST'])
+@login_required
+def update(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author!=current_user:
+        #403 error is forbidden
+        abort(403)
+    form  = PostForm()
+
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.body = form.body.data
+        db.session.commit()
+        flash('Post Updated')
+        return render_template('blog/posts.post', post_id = post.id)
+
+    elif request.method = 'GET':
+        form.title.data = post.title
+        form.body.data = post.body
+
+    return render_template('blog/create_post.html',title = 'Updating' form =form)
+
+
+@blog.route('blog/<int:post_id>/delete', methods = ['GET', 'POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author!=current_user:
+        abort(403)
+
+    db.session.delete(post)
+    db.session.commit()
+
+    flash('Post deleted.')
+    return redirect(url_for('main.index'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# DELETE 
+#
