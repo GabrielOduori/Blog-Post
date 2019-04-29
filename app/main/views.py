207523328bb29b04b19b954a1246985b32fdb5ae
Gabriel Oduori <gabriel.oduori@gmail.com>
2019-04-29 18:28:31 +0300

    Added function to comment on a post

--- app/main/views.py
+++ app/main/views.py
@@ -35,3 +35,31 @@ def info():
     Infor page
     '''
     return render_template('info.html')
+
+
+
+# Users profile page
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+    posts = user.posts.order_by(Post.timestamp.desc()).all()
+    return render_template("profile/profile.html", user = user, posts = posts)
+
+
+@main.route('/post/<int:id>', methods = ['GET','POST'])
+def post(id):
+    post = Post.query.get_or_404
+    form_comment = CommentForm()
+
+    if form_comment.validate_on_submit():
+        comment = Comment(body = form_comment.body.data,
+                          post = post,
+                          author = current_user)
+        db.session.add(comment)
+        db.session.commit()
+        flash('You comments has been added.')
+        return redirect(url_for('.post', id=post.id))
+    return render_template('post.html',posts = post, form_comment=form_comment, comment = comment)
