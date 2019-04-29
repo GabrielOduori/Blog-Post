68b7fad56449db0dd8122c93f84fd6fab6e1aea4
Gabriel Oduori <gabriel.oduori@gmail.com>
2019-04-29 23:25:16 +0300

    Added a post form for the posts

---
+++ app/blogs/forms.py
@@ -0,0 +1,9 @@
+# blog_posts/forms.py
+from flask_wtf import FlaskForm
+from wtforms import StringField,SubmitField,TextAreaField
+from wtforms.validators import DataRequired
+
+class PostForm(FlaskForm):
+    title = StringField('Title',validators=[DataRequired()])
+    body = TextAreaField('Text',validators=[DataRequired()])
+    submit = SubmitField("Post")
