709dda37e671eb07ca87be6274d5da56e7cbb5c1
Gabriel Oduori <gabriel.oduori@gmail.com>
2019-04-30 15:10:43 +0300

    Added functionalities for CRUD Operatiosn on blogs view.py

--- app/blogs/views.py
+++ app/blogs/views.py
@@ -1,40 +1,42 @@
-from flask import render_template, url_for, request, redirect
+from flask import render_template, url_for, request, redirect, flash
 from flask_login import current_user, login_required
 from app import db
-from app.models import Blog, Post
+from app.models import Blog, User
 from app.blogs.forms import PostForm
+from app.blogs import blogs
+
 
 
 # CREATE A BLOG
 
-@blog.route('/create', methods = ['GET', 'POST'])
+@blogs.route('/create', methods = ['GET', 'POST'])
 @login_required
 def create_post():
     post_form = PostForm()
     if post_form.validate_on_submit():
-        post = Blog(title = form.title.data,
-                    body = form.body.data,
+        post = Blog(title = post_form.title.data,
+                    body = post_form.body.data,
                     user_id = current_user.id)
         db.session.add(post)
         db.session.commit()
         flash('Post saved')
-        return redirect(url_for('main.html'))
+        return redirect(url_for('main.index'))
     return render_template('blog/create_post.html', post_form=post_form)
 
 
 # VIEW A POST
-@blog.route('blog/<int:post_id>')
+@blogs.route('blog/<int:post_id>')
 def post(post_id):
-    post = Post.query.get_or_404(post_id)
-    return render_template('post.html', title =post.title, date = post.timestamp, post = post)
+    post = Blog.query.get_or_404(post_id)
+    return render_template('user_post.html', title =post.title, date = post.timestamp, post = post)
 
 # UPDATE
 
 
-@blog.route('blog/<int:post_id>/update', methods = ['GET', 'POST'])
+@blogs.route('blog/<int:post_id>/update', methods = ['GET', 'POST'])
 @login_required
 def update(post_id):
-    post = Post.query.get_or_404(post_id)
+    post = Blog.query.get_or_404(post_id)
     if post.author!=current_user:
         #403 error is forbidden
         abort(403)
@@ -47,17 +49,17 @@ def update(post_id):
         flash('Post Updated')
         return render_template('blog/posts.post', post_id = post.id)
 
-    elif request.method = 'GET':
+    elif request.method == 'GET':
         form.title.data = post.title
         form.body.data = post.body
 
-    return render_template('blog/create_post.html',title = 'Updating' form =form)
+    return render_template('blog/create_post.html',title = 'Updating', form = form)
 
 
-@blog.route('blog/<int:post_id>/delete', methods = ['GET', 'POST'])
+@blogs.route('blog/<int:post_id>/delete', methods = ['GET', 'POST'])
 @login_required
 def delete_post(post_id):
-    post = Post.query.get_or_404(post_id)
+    post = Blog.query.get_or_404(post_id)
     if post.author!=current_user:
         abort(403)
 
@@ -66,19 +68,3 @@ def delete_post(post_id):
 
     flash('Post deleted.')
     return redirect(url_for('main.index'))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# DELETE 
-#
