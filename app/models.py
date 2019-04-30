b9f5bb062e2de1ec266dcba2c5edd577c21b8054
Gabriel Oduori <gabriel.oduori@gmail.com>
2019-04-30 18:50:31 +0300

    Added mixin objects to the User class

--- app/models.py
+++ app/models.py
@@ -5,6 +5,7 @@ from werkzeug.security import generate_password_hash,check_password_hash
 from . import login_manager
 from flask_login import UserMixin, LoginManager
 from datetime import datetime
+from flask_login import UserMixin
 
 
 
@@ -13,7 +14,7 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 
-class User(db.Model):
+class User(UserMixin,db.Model):
     '''
     class for users
     '''
@@ -53,9 +54,9 @@ class Blog(db.Model):
     title = db.Column(db.String(120), nullable=False)
     body = db.Column(db.Text, nullable = False)
     
-    def __init__(self,title, text, user_id):
+    def __init__(self,title, body, user_id):
         self.title = title
-        self.text = text
+        self.body = body
         self.user_id = user_id
         
     def __repr__(self):
