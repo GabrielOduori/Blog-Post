d115d5eaf38332a7dfb8fe93b65012902819f04d
Gabriel Oduori <gabriel.oduori@gmail.com>
2019-05-01 23:37:48 +0300

    Upfated comments rendering function on the blogs views and fadded comments form on the blogs form.py file

--- app/blogs/views.py
+++ app/blogs/views.py
@@ -1,8 +1,8 @@
 from flask import render_template, url_for, request, redirect, flash
 from flask_login import current_user, login_required
 from app import db
-from app.models import Blog, User
-from app.blogs.forms import PostForm
+from app.models import Blog, User, Comment
+from app.blogs.forms import PostForm, CommentForm
 from app.blogs import blogs
 
 
@@ -30,30 +30,32 @@ def post(post_id):
     post = Blog.query.get_or_404(post_id)
     return render_template('blog/post.html', title =post.title, date = post.timestamp, post = post)
 
-# UPDATE
 
 
 @blogs.route('blog/<int:post_id>/update', methods = ['GET', 'POST'])
 @login_required
 def update(post_id):
+    '''
+    function to update post
+    '''
     post = Blog.query.get_or_404(post_id)
     if post.author!=current_user:
         #403 error is forbidden
         abort(403)
-    form  = PostForm()
+    post_form  = PostForm()
 
-    if form.validate_on_submit():
-        post.title = form.title.data
-        post.body = form.body.data
+    if post_form.validate_on_submit():
+        post.title = post_form.title.data
+        post.body = post_form.body.data
         db.session.commit()
         flash('Post Updated')
-        return render_template('blog/posts.post', post_id = post.id)
+        return redirect(url_for('blogs.post', post_id=post.id))
 
     elif request.method == 'GET':
-        form.title.data = post.title
-        form.body.data = post.body
+        post_form.title.data = post.title
+        post_form.body.data = post.body
 
-    return render_template('blog/create_post.html',title = 'Updating', form = form)
+    return render_template('blog/create_post.html',title = 'Updating', post_form = post_form)
 
 
 @blogs.route('blog/<int:post_id>/delete', methods = ['GET', 'POST'])
@@ -68,3 +70,25 @@ def delete_post(post_id):
 
     flash('Post deleted.')
     return redirect(url_for('main.index'))
+
+
+
+@blogs.route('/blog/<int:id>/comment', methods = ['GET','POST'])
+@login_required
+def comment(id):
+    post = Blog.query.get_or_404(id)
+    form_comment = CommentForm()
+
+    if form_comment.validate_on_submit():
+        comment = Comment(body = form_comment.body.data,
+                            blog_id = post.id,
+                          author_id= current_user.id)
+        db.session.add(comment)
+        db.session.commit()
+        flash('Comments added successfuly!')
+        return redirect(url_for('.post', id=post.id))
+
+    from pprint import pprint
+    pprint(post)
+    return render_template('blog/post.html',post =post, form_comment=form_comment)
+    
