06b0c4fe739fd03d4cd7e32a19d451128476754c
Gabriel Oduori <gabriel.oduori@gmail.com>
2019-05-02 00:48:55 +0300

    Added database URI to the config files and test class to manage.py file

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 from app import create_app, db
 from flask_script import Manager, Server
-from app.models import User, Blog
+from app.models import User, Blog, Comment
 from flask_migrate import Migrate, MigrateCommand
 
 app = create_app('development')
@@ -11,8 +11,17 @@ manager.add_command('db',MigrateCommand)
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app, db = db, User = User, Blog = Blog)
+    return dict(app = app, db = db, User = User, Blog = Blog, Comment = Comment)
 
+
+@manager.command 
+def test():
+    '''
+    Running Unit tests
+    '''
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
 
 if __name__=='__main__':
     manager.run()
