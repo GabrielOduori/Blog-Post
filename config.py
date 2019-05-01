9f88fdb4d42e9fb0b91ee5bbc1b3f3a0d60edd14
Gabriel Oduori <gabriel.oduori@gmail.com>
2019-05-02 00:36:17 +0300

    Updated configuration file

--- config.py
+++ config.py
@@ -1,17 +1,26 @@
 import os
 
 class Config:
-    # SECRET_KEY=os.environ.get('SECRET_KEY')
-    SECRET_KEY='Ghdks87PlsxCvTx'
+    SECRET_KEY=os.environ.get('SECRET_KEY')
+    # SECRET_KEY='Ghdks87PlsxCvTx'
     QUOTES_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:postgres@localhost/bloglist'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
-
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    
     
 class ProdConfig(Config):
     pass
     
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:postgres@localhost/bloglist_test'
 
 class DevConfig(Config):
     DEBUG = True
@@ -20,5 +29,6 @@ class DevConfig(Config):
         
 config_options = { 
     'development' : DevConfig,
-    'production' : ProdConfig
+    'production' : ProdConfig,
+    'test':TestConfig
 }
